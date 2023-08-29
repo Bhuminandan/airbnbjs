d4411ae586239b138e45deb09c4ac9759a50e5b9
Bhumi <bhumione9545@gmail.com>
2023-08-29 19:40:29 +0530

    Adding home page design half

---
+++ app.js
@@ -0,0 +1,17 @@
+const mobileNavDiv = document.querySelector(".mobile-nav");
+const accountMenuButton = document.querySelector(".menu-icon");
+
+
+// Mobile Nave bar functionality
+let isMobNavOpen = false;
+accountMenuButton.addEventListener("click", () => {
+    if (isMobNavOpen) {
+        mobileNavDiv.style.display = "none";
+        isMobNavOpen = false;
+        return;
+    }
+
+    mobileNavDiv.style.display = "flex";
+    isMobNavOpen = true;
+
+})
