4932d14c05f3f3f5587fa337efc01c3933413618
Bhumi <bhumione9545@gmail.com>
2023-08-31 19:22:29 +0530

    Adding changes

--- listings/listings.js
+++ listings/listings.js
@@ -1,9 +1,15 @@
 const mobileNavDiv = document.querySelector(".mobile-nav");
 const accountMenuButton = document.querySelector(".menu-icon");
+const showMapBtn = document.querySelector(".showmap");
+const mapContainer = document.querySelector(".right-map");
+const cardsContainer = document.querySelector(".left-listings");
+
+// Global Variables
+let isMobNavOpen = false;
+let isMapOpen = false;
 
 
 // Mobile Nave bar functionality
-let isMobNavOpen = false;
 accountMenuButton.addEventListener("click", () => {
     if (isMobNavOpen) {
         mobileNavDiv.style.display = "none";
@@ -13,5 +19,19 @@ accountMenuButton.addEventListener("click", () => {
 
     mobileNavDiv.style.display = "flex";
     isMobNavOpen = true;
+});
 
-})
+showMapBtn.addEventListener("click", () => {
+    if (isMapOpen) {
+        mapContainer.style.display = "none";
+        cardsContainer.style.display = "block";
+        isMapOpen = false;
+        return;
+    }
+
+    mapContainer.style.display = "block";
+    cardsContainer.style.display = "none";
+    isMapOpen = true;
+});
+
+
